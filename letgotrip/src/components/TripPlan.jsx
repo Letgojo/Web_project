6ecdf065d15d206b171c4ea66ba88bff83bfa52b
lovaoi777 <77834532+lovaoi777@users.noreply.github.com>
2022-10-25 14:08:09 +0900

    예

--- letgotrip/src/components/TripPlan.jsx
+++ letgotrip/src/components/TripPlan.jsx
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 
 const Triptitle = styled.div`
     background-color : white;
-    margin : 100px auto;
+    margin : 100px auto ;
+    margin-bottom : 0px;
     width : 500px;
     height : 60px;
     font-size : 30px;
@@ -11,12 +12,38 @@ const Triptitle = styled.div`
     border-radius : 30px;
     padding-top : 20px;     
 `
+const TripYes = styled.div`
+    background-color : white;
+    margin : 50px auto;
+    margin-bottom : 0px;
+    width : 500px;
+    height : 60px;
+    font-size : 30px;
+    text-align : center;
+    border-radius : 30px;
+    padding-top : 20px;  
+    cursor : pointer; 
+    `
+const TripNo = styled.div`
+    background-color : white;
+    margin : 50px auto;
+    margin-bottom : 0px;
+    width : 500px;
+    height : 60px;
+    font-size : 30px;
+    text-align : center;
+    border-radius : 30px;
+    padding-top : 20px;   
+    cursor : pointer;
+`
 const TripPlan = () => {
     return (
         <>
             <Triptitle>
                 여행지를 정하셨나요 ? 
             </Triptitle>
+            <TripYes>예</TripYes>
+            <TripNo>아니요</TripNo>
         </>
     );
 };
