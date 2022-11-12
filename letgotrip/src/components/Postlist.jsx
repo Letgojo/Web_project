2714815da05ced97d77cd465b581dc6922732e7d
lovaoi777 <77834532+lovaoi777@users.noreply.github.com>
2022-11-12 14:18:27 +0900

    배포

--- letgotrip/src/components/Postlist.jsx
+++ letgotrip/src/components/Postlist.jsx
@@ -1,25 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import '../font/fontstyle.css';
+import trash from '../img/bin.png'
+import { firestore,storage } from '../firebase.js'
+import { useNavigate } from 'react-router-dom';
 const ListForm = styled.div`
     display : flex;
+    width : 1100px;
     margin-bottom : 30px;
     font-family: 'HallymGothic-Regular';
+    border : 1px solid black;
 `
 const ListImg = styled.div`
     width :282px;
     height : 352px;
     border : 1px solid black;
 `
+const Image = styled.img`
+    width :282px;
+    height : 352px;
+    `
 const ListText = styled.div`
     text-align : left;
     margin-left : 40px;
 `
-const Postlist = ({name,title,content}) => {
+const TrashImage = styled.img`
+    width : 20px;
+    height : 30px;
+    text-align : right;
+    cursor : pointer;
+`
+const Postlist = ({Url,name,title,content}) => {
+    const navigate = useNavigate();
+    const handleDel = () => {
+        navigate('/');
+        console.log(title,"삭제되었습니다")
+        const db = firestore.collection("게시글").doc(title).delete()
+        return db
+        
+    }
     return (
         <ListForm>
             <ListImg>
-                111
+                <Image src={Url} alt="사진" />
             </ListImg>
             <ListText>
             작성자 :  {name}
@@ -28,6 +51,7 @@ const Postlist = ({name,title,content}) => {
             <br />
             내용 : {content}
             </ListText>
+            <TrashImage onClick={handleDel} src={trash} alt="" />
         </ListForm>
     );
 };
