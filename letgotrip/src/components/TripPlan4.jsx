6b30bf5e736d7628ceb7773a22ebba27decb0451
lovaoi777 <77834532+lovaoi777@users.noreply.github.com>
2022-11-15 03:54:14 +0900

    파이어베이스연결

--- letgotrip/src/components/TripPlan4.jsx
+++ letgotrip/src/components/TripPlan4.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import styled from 'styled-components';
+import HotelList from './HotelList'
 import guesthouse from '../img/숙소사진/guesthouse.png'
 import hanok from '../img/숙소사진/hanok.png'
 import hotel from '../img/숙소사진/hotel.png'
@@ -7,9 +8,10 @@ import motel from '../img/숙소사진/motel.png'
 import deleteBtn from '../img/숙소사진/x_button.png'
 import pension from '../img/숙소사진/island.png'
 import '../font/fontstyle.css';
+import { firestore} from '../firebase.js'
 const Template = styled.div`
 width : 1300px;
-height : 1200px;
+max-height : 100%;
 border : 1px solid black;
 background-color : white;
 margin : 5% auto;   
@@ -18,8 +20,8 @@ padding-left : 40px;
 font-family: 'HallymGothic-Regular';
 `
 const Btnimg = styled.img`
-    width : 98px;
-    height : 98px;
+    width : 90px;
+    height : 70px;
 `
 const CategoryConent = styled.div`
     width : 1000px;
@@ -30,21 +32,21 @@ const CategoryConent = styled.div`
 `
 const Category = styled.div`
     width : 100px;
-    height : 150px;
+    height : 100px;
     text-align:center;
     cursor : pointer;
 `
 const CategoryType = styled.div`
     border : 1px solid black;
-    width : 545px;
-    height : 89px;
+    width : 450px;
+    height : 50px;
     border-radius : 30px;
     display : flex;
 `
 const TypeRadio = styled.div`
-    margin-top : 30px;
+    margin-top : 10px;
     margin-left : 40px;
-    font-size: 35px;
+    font-size: 20px;
 `
 const Typeinput = styled.input`
     margin-left : 10px;
@@ -52,15 +54,50 @@ const Typeinput = styled.input`
     
 `
 const TripPlan4 = () => {
+    const [PostList, setPostList] = useState([]);
+    let sessionStorage = window.sessionStorage;
+    const handleChoiceGH = () => { 
+        sessionStorage.setItem("숙소","게스트하우스")
+    }
+    const handleChoicePH = () => { 
+        sessionStorage.setItem("숙소","펜션")
+    }
+    const handleChoiceHT = () => { 
+        sessionStorage.setItem("숙소","호텔")
+    }
+    const handleChoiceMT = () => { 
+        sessionStorage.setItem("숙소","모텔")
+    }
+    const handleChoiceKH = () => { 
+        sessionStorage.setItem("숙소","한옥")
+    }
+    // sessionStorage.getItem("숙소") /숙소/호텔/부산/2022-11-10/2022-11-13
+    useEffect(()=>{
+    const db = firestore.collection("숙소").doc("호텔").collection("부산").doc("2022-11-10").collection("2022-11-13");
+    let person = [];
+    db.get().then((result)=>{
+        result.forEach((allDoc)=>{
+            person.push(allDoc.data())
+        })
+  })
+  setTimeout(()=>{
+    console.log(PostList)
+    person.map((element)=>(
+        setPostList((PostList)=> [
+            ...PostList,
+            {name:element.숙박업체, money:element.금액,Url:element.사진url}
+        ])))
+    },1000)
+},[setPostList])
     return (
         <Template>
             <CategoryConent>
             <Category><Btnimg src={deleteBtn} alt="해당없음"/>해당없음</Category>
-            <Category><Btnimg src={guesthouse} alt="게스트하우스"/>게스트하우스</Category>
-            <Category><Btnimg src={pension} alt="펜션"/>펜션</Category>
-            <Category><Btnimg src={hotel} alt="호텔"/>호텔</Category>
-            <Category><Btnimg src={motel} alt="모텔"/>모텔</Category>
-            <Category><Btnimg src={hanok} alt="한옥"/>한옥</Category>
+            <Category onClick={handleChoiceGH}><Btnimg src={guesthouse} alt="게스트하우스"/>게스트하우스</Category>
+            <Category onClick={handleChoicePH}><Btnimg src={pension} alt="펜션"/>펜션</Category>
+            <Category onClick={handleChoiceHT}><Btnimg src={hotel} alt="호텔"/>호텔</Category>
+            <Category onClick={handleChoiceMT}><Btnimg src={motel} alt="모텔"/>모텔</Category>
+            <Category onClick={handleChoiceKH}><Btnimg src={hanok} alt="한옥"/>한옥</Category>
             </CategoryConent>
             <hr />
             <CategoryType>
@@ -68,6 +105,20 @@ const TripPlan4 = () => {
                 <TypeRadio><Typeinput type="radio" name="type" style={{width:"25px",height:"25px"}}/>가격</TypeRadio>
                 <TypeRadio><Typeinput type="radio" name="type" style={{width:"25px",height:"25px"}}/>특가</TypeRadio>
             </CategoryType>
+            <div>
+                <ul> 
+                    {PostList.map((element,index)=>(
+                        <HotelList 
+                            key={index}
+                            name={element.name}
+                            money={element.money}
+                            Url={element.Url}
+                        />
+                        ))}
+                        
+                </ul>
+            </div>
+
         </Template>
     );
 };
