9efdd44b13ed384d819633d617ec6131ce49d231
lovaoi777 <77834532+lovaoi777@users.noreply.github.com>
2022-11-17 02:02:08 +0900

    댓글창

--- letgotrip/src/components/CommunityDetail.jsx
+++ letgotrip/src/components/CommunityDetail.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React,{useEffect}from 'react';
 import styled from 'styled-components';
 import { useParams,useLocation } from "react-router"
 import Postlist from './Postlist';
 import { useState } from 'react';
-
+import User from '../img/user.png'
+import { firestore } from '../firebase.js'
+import CommentsList from './CommentsList';
 const Template  = styled.div`
     width : 1300px;
     height : 1200px;
@@ -13,27 +15,114 @@ const Template  = styled.div`
     border-radius : 30px;
     font-family: 'HallymGothic-Regular';
 `
-const CommunityDetail = () => {
+const HeaderDiv = styled.div`
+    display : flex;
+    justify-content : space-between;
+    margin : 30px 30px 0 70px;
+`
+const SectionDiv = styled.div`
+    margin-top : 30px;
+    margin-left : 70px;
+`
+const SectionHeader = styled.div`
+
+`
+const SectionMain = styled.div`
+    margin-top : 10px;
+`
+const Sectionimg = styled.img`
+    margin-top : 30px; 
+    width :235px;
+    height : 185px;
+`
+const Comments = styled.div`
+    margin-top : 30px;
+    margin-left : 70px;
+    border: 1px solid black;
+    width : 600px
+`
+const CommentSpan = styled.span`
+    border-left: 1px solid black
+`
+const CommentInput = styled.input`
+    border : 0px;
+    width : 563px
+`
+const Commentlist = styled.div`
+    margin-top : 30x;
+    margin-left: 70px
+`
+const CommunityDetail =  () => {
     const [PostList , setPostList] = useState([]);
-    const {id} = useParams();
     const location = useLocation();
     const State = location.state
-    const user = [] ;
-    user.push(State[0])
-    console.log(user[0].Url)
-    // State.map((element)=>
-    //     setPostList((PostList)=>[
-    //         ...PostList,
-    //         {name:element.작성자, title:element.제목, content:element.내용 ,Url:element.이미지URL ,upload:element.업로드날짜}
-    //     ])
-    // )
+    console.log(State); 
+    let user = [];
+
+    try {
+    setTimeout(async()=>{
+    const db = firestore.collection("게시글").doc(State.id).collection("댓글");
+    await db.get().then((result)=>{
+        result.forEach((allDoc)=>{
+            user.push(allDoc.data())
+        })
+    })
+})
+    }
+    catch{
+        console.log("error")
+    }
+
+    useEffect(()=>{
+        setTimeout(()=>{
+        user.map((element)=>(
+            setPostList((Postlist)=> [
+                ...Postlist,
+                {name : element.작성자, content : element.내용},
+            ])))
+        },1000)
+    },[])
+const Upload = async () => { 
+    var count =  user.length.toString();
+    let sessionStorage = window.sessionStorage;
+    const comment = document.getElementById('com').value
+    console.log(comment)
+    console.log(State.id)
+    const bucket = firestore.collection("게시글").doc(State.id).collection("댓글").doc(count);
+            await bucket.set({
+                "번호" : count,
+                "작성자" : sessionStorage.getItem("name"), 
+                "내용" : comment,
+            })
+            console.log("업로드 성공");
+}
+
     return (
         <Template>
-            <div>{user[0].name}</div>
+            <HeaderDiv>
+            <div>작성자 : {State.name}</div>
+            <div>{State.upload}</div>
+            </HeaderDiv>
+            <SectionDiv>
+            <SectionHeader>제목 : {State.title}</SectionHeader>
+            <SectionMain>내용 : {State.content}</SectionMain>
+            <Sectionimg src={State.Url} alt="사진" />
+            </SectionDiv>
             <br />
             <br />
-            <div>{user[0].title}</div>
-            <img src={user[0].Url} alt="사진" />
+            <hr />
+            <Comments><CommentInput type="text" placeholder='댓글을 입력하세요' id='com'/><CommentSpan onClick={Upload}>등록</CommentSpan></Comments>
+            <Commentlist>
+                <ul>
+                    {PostList.map((element,index)=>(
+                        <CommentsList
+                            key={index}
+                            name={element.name}
+                            comment={element.content}
+                        />
+                    ))}
+                </ul>
+            </Commentlist>
         </Template>
     );
 };
